cd4703f1d59dbb2c9ddc15af69ed13c76715eeb1
unknown <relh@umd.edu>
2015-04-11 15:53:22 +0100

    beginning of AI

---
+++ Assets/Scripts/AI/UnitBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitBehavior : MonoBehaviour {
+	
+	public float movementSpeed = 1;
+	public Vector3 direction = Vector3.forward;
+
+	enum States {moving, detected, attacking};
+	public States state;
+
+	// Use this for initialization
+	void Start () {
+		state = States.moving;
+	}
+
+	void Update(){
+		Vector3 fwd = transform.TransformDirection(direction);
+		if (Physics.Raycast(transform.position, fwd, 10))
+			print("There is something in front of the object!");
+
+		transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+	}
+}
