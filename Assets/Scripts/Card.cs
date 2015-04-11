38f2204f2c9f313ace211b1cd8cf2e8939333e90
Matt Favero <mattfavero@gmail.com>
2015-04-11 11:05:06 -0400

    initial

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+
+	private Vector3 _startPos;
+
+	// Use this for initialization
+	void Start () {
+		_startPos = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LookingAt(GameObject looker) {
+		var dest = Vector3.Lerp(_startPos, looker.transform.position, 0.33f);
+
+	}
+
+	void StopLookingAt() {
+		transform.position = _startPos;
+	}
+}
---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+
+	public GameObject CrosshairSprite;
+	private Vector3 _startPos;
+	private GameObject LastLookingAt;
+
+	// Use this for initialization
+	void Start () {
+		_startPos = CrosshairSprite.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var hit = new RaycastHit();
+		if (Physics.Raycast(this.transform.position, transform.forward, out hit, 10f)) {
+			CrosshairSprite.transform.position = hit.point;
+
+			var go = hit.collider.gameObject;
+
+			// Stop looking at previous object
+			if (go != LastLookingAt && LastLookingAt != null) {
+				LastLookingAt.SendMessage("StopLookingAt", SendMessageOptions.DontRequireReceiver);
+			}
+			// Look at current object
+			go.SendMessage("LookingAt", gameObject, SendMessageOptions.DontRequireReceiver);
+			LastLookingAt = go;
+		}
+		// Not looking at anything
+		else if (LastLookingAt != null) {
+			LastLookingAt.SendMessage("StopLookingAt", SendMessageOptions.DontRequireReceiver);
+			LastLookingAt = null;
+		}
+	}
+
+}
