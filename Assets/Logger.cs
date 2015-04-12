81c16c5b0c799aaa855dfc646fc66569d5fe7eeb
Shariq Hashme <shariqhashme@gmail.com>
2015-04-11 22:59:31 -0700

    tuff

---
+++ Assets/Logger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Logger : MonoBehaviour {
+
+	public GameObject TrackedObject2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (KinectStream.Instance.data.Count != 1)
+			return;
+		Debug.Log (KinectStream.Instance.getPlayer ().getJoint ("Head").ToString() + ":" + TrackedObject2.transform.position.ToString() + ":" + (TrackedObject2.transform.position -this.gameObject.transform.position).ToString());
+	}
+}
